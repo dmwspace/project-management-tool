81496044e1ce7803757fcad81d30103bcea383ef
Dean <dmwspace@gmail.com>
2024-03-22 15:55:45 -0700

    more troubleshooting

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8000'
+    }
+  },
+  plugins: [react()],
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: "./src/main.jsx",
+    },
+  },
+});
