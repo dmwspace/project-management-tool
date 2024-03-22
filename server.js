7a01ab5b9e548850e8be24ff670ee0567eb170e7
Dean <dmwspace@gmail.com>
2024-03-22 15:33:58 -0700

    Render IO

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ require("./config/database");
 
 const app = express();
 
+app.set('view engine', "ejs")
 // add in when the app is ready to be deployed
 // app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(logger("dev"));
@@ -25,9 +26,21 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/projects", require("./routes/api/projects"))
 
 // "catch all" route
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+if(process.env.IS_PRODUCTION){
+  // This code will run in production
+    const manifest = require('./dist/manifest.json');
+    app.use(express.static(path.join(__dirname, "dist")));
+  
+    // "catch all" route when the code is in production
+    app.get('/*', function(req, res) {
+      res.render(path.join(__dirname, 'dist', 'index.ejs'), {manifest});
+    });
+  }
+  
+  // This is the catch all when the code is running locally
+  app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, './','index.html'));
+  });
 
 
 const port = process.env.PORT || 3001;
