74c492ff7e2e8586dcb5ac8075df521a26c6d3c5
Dean <dmwspace@gmail.com>
2024-03-22 15:36:30 -0700

    testing index

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(require("./config/auth"));
 // api routes must be before the "catch all" route
 app.use("/api/users", require("./routes/api/users"));
-app.use("/api/projects", require("./routes/api/projects"))
+app.use("/api/projects", require("./routes/api/more"))
 
 // "catch all" route
 if(process.env.IS_PRODUCTION){
